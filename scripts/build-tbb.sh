61a364477f8b15d0b43eab3abea7a176b6fe1adf
Valery Matskevich <generalretcher@gmail.com>
2024-10-08 16:01:44 +0300

    disable strictness
    Signed-off-by: Valery Matskevich <generalretcher@gmail.com>

--- scripts/build-tbb.sh
+++ scripts/build-tbb.sh
@@ -3,7 +3,7 @@ cd oneTBB
 mkdir -p build
 mkdir -p ../thd/libs/
 
-cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DTBB_TEST=OFF
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DTBB_TEST=OFF -DTBB_STRICT=OFF
 cmake --build build --parallel
 cmake --install build --prefix ../thd/libs
 
