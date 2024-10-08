b00053fe44b3b036168fe809629a63a8adc45546
Valery Matskevich <generalretcher@gmail.com>
2024-10-08 15:40:42 +0300

    add eigen and omp
    Signed-off-by: Valery Matskevich <generalretcher@gmail.com>

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,5 +1,5 @@
 mkdir -p results
 cd pbbsbench
-OMP_NUM_THREADS=$1 ./run_benches.py --dir ../results --eigen --omp --tbb --tbb-path $PWD/../thd/libs
+OMP_NUM_THREADS=$1 ./run_benches.py --dir ../results --eigen --omp --tbb --tbb-path $PWD/../thd/libs --only integerSort/parallelRadixSort
 cd ../
 
