c2498eb489d3ea2efba7df640e2ab57979c847fa
Valery Matskevich <generalretcher@gmail.com>
2024-10-08 15:21:21 +0300

    it finally works (yay)
    Signed-off-by: Valery Matskevich <generalretcher@gmail.com>

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,3 +1,5 @@
 mkdir -p results
 cd pbbsbench
-OMP_NUM_THREADS=$1 ./run_benches.py --dir ../results --tbb --tbb-path ../thd/libs --only integerSort/parallelRadixSort --mode TBB_AUTO --small
+OMP_NUM_THREADS=$1 ./run_benches.py --dir ../results --tbb --tbb-path $PWD/../thd/libs --only integerSort/parallelRadixSort --mode TBB_AUTO --small
+cd ../
+
