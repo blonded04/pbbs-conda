6c04ccac95de2d865596f096ec6d55728cac43ec
Valery Matskevich <39431974+blonded04@users.noreply.github.com>
2024-10-16 14:46:11 +0300

    Update run-all.sh

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -x
 
+set -e
+source activate base
+conda activate base
+conda activate benchmarks
+
 mkdir -p results
 cd pbbsbench
 OMP_NUM_THREADS=$1 ./run_benches.py --dir $PWD/../results --eigen --omp --tbb --tbb-path $PWD/../thd/libs --only integerSort/parallelRadixSort
 cd ../
+
+conda deactivate
