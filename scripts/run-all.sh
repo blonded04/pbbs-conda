a81b9f95496bd88226b4723f365c2d41aa974a3b
Valery Matskevich <39431974+blonded04@users.noreply.github.com>
2024-10-16 14:38:04 +0300

    Update run-all.sh

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,5 +1,6 @@
+#!/bin/bash -x
+
 mkdir -p results
 cd pbbsbench
 OMP_NUM_THREADS=$1 ./run_benches.py --dir $PWD/../results --eigen --omp --tbb --tbb-path $PWD/../thd/libs --only integerSort/parallelRadixSort
 cd ../
-
